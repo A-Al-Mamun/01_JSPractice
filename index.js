17f0eded93c5bb386cc81a8e8902e5a6474bfcab
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-27 17:56:46 +0600

    01 JavaScript Learn session

---
+++ index.js
@@ -0,0 +1,68 @@
+// Lesson 01 | JavaScript Basics
+/************************************** */
+
+// 1a. Use Alert to display "Good Morning" in s popup
+
+// alert("Good Morning!");
+
+
+// 1b. Display your name in a popup
+
+// alert("Abdullah Al Mamun");
+
+
+// 1c. Using Math, calculate 10 +5 in the console
+
+// alert(10+5);
+// console.log(10+5);
+
+// 1d. Calculate 20 - 5 in the console
+
+// console.log(20 - 5);
+
+// 1e. Calculate 2 + 2 - 5 in the console
+
+// console.log(2 + 2 - 5);
+
+// 1f. Use document.body.innerHTML = ...; to display 'Good Morning!' on the web page.
+
+
+// Lesson 02 | JavaScript Basics
+
+
+// Exercises
+// 2a. At a resturant, you order 1 soup for $10, 3 burguers for $8 each, 
+//     and 1 ice cream for $5. 
+//     Use JavaScipt to calculate the cost of the order.
+
+// 2b. You're at a resturant with 2 friends(3 people in total) and make the 
+//     same order as 2a. Calculate how much each perosn Pays.
+
+// 2c. Calculate the total cost of a toaster ($18.50) and 2 shirts ($7.50 each).
+
+// 2d. Calcualate a 10% tax for the total in exercise 2c.
+
+// 2e. Calculate a 20% tax for the total in 2c (remember that 1% = 1/100,
+//      so 20% = 20 / 100 = 0.2). 
+
+
+
+// Lesson 03 | Javascripts Bascis
+
+// Text
+
+// alert('Hello ' + 'World!');
+
+// Baskerball = $20.95
+// T-shirt = $7.99
+// Total 2 items Price = $28.94
+// alert('I\'m Mamun\nI have Item (' +(1+1) + '): $' + ((2095 + 799)/100));
+
+alert(`Item (${1+1}): $${(2095+799)/100}`);
+
+
+
+// Lesson 04 | JavaScript Basic
+
+
+
