a9b48f41506022a7f4b86242407d012000b31de6
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-09-16 22:44:47 +0600

    Arithmatic operation practice by my  new example creating

--- JS_Basic/app.js
+++ JS_Basic/app.js
@@ -3,25 +3,61 @@
 
 // Variable data type Practice
 // var let const
-var name = "Abdullah Al Mamun"; //String Variable
-var age = 28; //Number Variable
-var hight = 5.10; //Number Variable
-let isMale = true; //Boolean
-let isFemale = false;
-let gendar = ((isMale = true)? isMale : isFemale )
+// var name = "Abdullah Al Mamun"; //String Variable
+// var age = 28; //Number Variable
+// var hight = 5.10; //Number Variable
+// let isMale = true; //Boolean
+// let isFemale = false;
+// let gendar = ((isMale = true)? isMale : isFemale )
 
-let hobbies = ['Coding', 'Learning', 'Waching']; //Array but type object
-let numbers = {
-    num1: '01309068352',
-    num2: '01310107526'
+// let hobbies = ['Coding', 'Learning', 'Waching']; //Array but type object
+// let numbers = {
+//     num1: '01309068352',
+//     num2: '01310107526'
+// }
+// let maritual;
+// let bb = null;
+// document.write("Name :" + name + " 'data type' " + typeof name +"<br>");
+// document.write("Age :" + age + " 'data type' " + typeof age + "<br>");
+// document.write("Hight :" + hight + " Feets"+ " 'data type' " + typeof hight +"<br>");
+// document.write("Gender Check : Male " + gendar + " 'data type' " + typeof isMale + " <br>");
+// document.write("Hobbies : " + hobbies + " 'data type' " + typeof hobbies + " <br>");
+// document.write("Numbers : " + numbers.num2 + " 'data type' " + typeof numbers + " <br>");
+// document.write("Maritual : " + maritual + " 'data type' " + typeof maritual + " <br>");
+// document.write("Bank Balance : " + bb + " 'data type' " + typeof bb + " <br>");
+
+
+//Arithmatic Operation Practice
+//+ Addition
+// - Subtractio
+// * Multiplication
+// / Division
+// % Modulas
+
+let num1 = 102;
+let num2 = 10;
+let result;
+
+result = num1+num2
+document.write("Number One : "+num1 + "<br>");
+document.write("Number Two : "+num2 + "<br><br>");
+if(num1 + num2){
+    result = num1 + num2;
+    document.write("Addition : " + result + "<br>");
 }
-let maritual;
-let bb = null;
-document.write("Name :" + name + " 'data type' " + typeof name +"<br>");
-document.write("Age :" + age + " 'data type' " + typeof age + "<br>");
-document.write("Hight :" + hight + " Feets"+ " 'data type' " + typeof hight +"<br>");
-document.write("Gender Check : Male " + gendar + " 'data type' " + typeof isMale + " <br>");
-document.write("Hobbies : " + hobbies + " 'data type' " + typeof hobbies + " <br>");
-document.write("Numbers : " + numbers.num2 + " 'data type' " + typeof numbers + " <br>");
-document.write("Maritual : " + maritual + " 'data type' " + typeof maritual + " <br>");
-document.write("Bank Balance : " + bb + " 'data type' " + typeof bb + " <br>");
+if(num1 - num2){
+    result = num1 - num2;
+    document.write("Subtraction : " + result + "<br>");
+}
+if(num1 * num2){
+    result = num1 * num2;
+    document.write("Multiplication : " + result + "<br>");
+}
+if(num1 / num2){
+    result = num1 / num2;
+    document.write("Division : " + result + "<br>");
+}
+if(num1 % num2){
+    result = num1 % num2;
+    document.write("Modulas : " + result + "<br>");
+}
